a91f60869e85779392c1f913c7a3f78c736dd8b6
Revarin <dan.perina2@gmail.com>
2021-11-05 09:30:10 +0100

    Add control buttons

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <the-layout/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TheLayout from './components/TheLayout.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { TheLayout }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+}
+* {
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/TheLayout.vue
@@ -0,0 +1,26 @@
+<!--
+    Main component, contains other components. 
+    TODO:
+        - Control buttons
+        - A lot
+!-->
+
+<template>
+    <div class="the-layout">
+        <control-button-row/>
+    </div>
+</template>
+
+<script>
+import ControlButtonRow from './controls/ControlButtonRow.vue'
+export default {
+    components: { ControlButtonRow },
+    name: "TheLayout",
+
+    
+}
+</script>
+
+<style>
+
+</style>
