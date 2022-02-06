6e6129b2d914af1cd88fe7a72b60437b0856198a
Revarin <dan.perina2@gmail.com>
2022-02-06 18:45:48 +0100

    Add code editor

--- doc/sources.md
+++ doc/sources.md
@@ -1,5 +1,5 @@
 # Sources
 
 ## Code editor
-[A Simple and easy to use CodeEditor for Vue.js](https://vuejsexamples.com/a-simple-and-easy-to-use-codeeditor-for-vue-js/)
 [Vue prism editor](https://github.com/koca/vue-prism-editor/tree/feature/next)
+[Prism highlighter](https://prismjs.com/)
--- src/components/TheLayout.vue
+++ src/components/TheLayout.vue
@@ -12,6 +12,7 @@
             </div>
             <div class="program-container">
                 <!-- TODO !-->
+                <prism-editor class="code-editor" v-model="code" :highlight="HighlightCode" line-numbers></prism-editor>
             </div>
         </div>
         <div class="model-container">
@@ -21,10 +22,18 @@
 </template>
 
 <script>
+// Prism Editor: https://github.com/koca/vue-prism-editor/tree/feature/next
+import { PrismEditor } from 'vue-prism-editor';
+import 'vue-prism-editor/dist/prismeditor.min.css';
+// Highlighting library: https://prismjs.com/
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-javascript';
+// My components
 import CommonButton from './common/CommonButton.vue'
+
 export default {
     name: "TheLayout",
-    components: { CommonButton },
+    components: { PrismEditor, CommonButton },
 
     data() {
         return {
@@ -34,12 +43,16 @@ export default {
                 {display: "||", funcName: "pause"},
                 {display: ">", funcName: "step"}
             ],
+            code: "",
         }
     },
 
     methods: {
         OnButtonClicked(functionName) {
             console.log("Button " + functionName + " clicked.");
+        },
+        HighlightCode(code) {
+            return highlight(code, languages.js);
         }
     }
 }
@@ -64,12 +77,23 @@ export default {
     width: 100%;
     height: 90%;
 
-    background: green;
+    background: darkslategray;
 }
 .model-container {
     width: 70%;
     height: 100%;
 
     background: blue;
+}
+
+.code-editor {
+    /* you must provide font-family font-size line-height. Example: */
+    font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
+    font-size: 14px;
+    line-height: 1.5;
+    padding: 5px;
+}
+.prism-editor__textarea:focus {
+    outline: none;
 }
 </style>
