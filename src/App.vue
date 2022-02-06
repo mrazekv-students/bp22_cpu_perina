caad64e3662f2532fb505e123a5644ee6e945a63
Revarin <dan.perina2@gmail.com>
2022-02-06 16:39:34 +0100

    Add main layout

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,43 @@
 <template>
-  <the-layout/>
+    <the-layout style="width: 100%; height: 100%;"/>
 </template>
 
 <script>
 import TheLayout from './components/TheLayout.vue'
 export default {
-  name: 'App',
-  components: { TheLayout }
+    name: 'App',
+    components: { TheLayout }
 }
 </script>
 
 <style>
-body {
-  margin: 0;
-  padding: 0;
-  width: 100%;
-  min-height: 100%;
-  background: #3A3A40;
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    width: 100%;
+    height: 100vh;
 }
 * {
-  box-sizing: border-box;
+    margin: 0;
+    box-sizing: border-box;
 }
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #D6D6D6;
+body {
+    margin: 0;
+    overflow: hidden;
+    display: flex;
+
+    border-style: dotted;
+    border-width: 2px;
+
+    background: #3A3A40;
+    color: #D6D6D6;
+}
+
+.horizontal-container {
+    display: flex;
+}
+.vertical-container {
+    display: initial;
 }
 </style>
--- src/components/TheLayout.vue
+++ src/components/TheLayout.vue
@@ -6,18 +6,69 @@
 !-->
 
 <template>
-    <div class="the-layout">
-        <control-button-row/>
+    <div class="horizontal-container">
+        <div class="vertical-container control-container">
+            <div class="horizontal-container button-container">
+                <control-button v-for="button in controlButtons" :key="button" :displayValue="button.display" :functionName="button.funcName"
+                    @button-clicked="OnButtonClicked"/>
+            </div>
+            <div class="program-container">
+                <!-- TODO !-->
+            </div>
+        </div>
+        <div class="model-container">
+            <!-- TODO !-->
+        </div>
     </div>
 </template>
 
 <script>
-import ControlButtonRow from './common/ControlButtonRow.vue'
+import ControlButton from './common/ControlButton.vue'
 export default {
     name: "TheLayout",
-    components: { ControlButtonRow },
+    components: { ControlButton },
+
+    data() {
+        return {
+            controlButtons: [
+                {display: "|>", funcName: "start"},
+                {display: "|||", funcName: "stop"},
+                {display: "||", funcName: "pause"},
+                {display: ">", funcName: "step"}
+            ],
+        }
+    },
+
+    methods: {
+        OnButtonClicked(functionName) {
+            console.log("Button " + functionName + " clicked");
+        }
+    }
 }
 </script>
 
 <style>
+.control-container {
+    width: 30%;
+    height: 100%;
+}
+.button-container {
+    width: 100%;
+    height: 10%;
+    justify-content: center;
+
+    background: red;
+}
+.program-container {
+    width: 100%;
+    height: 90%;
+
+    background: green;
+}
+.model-container {
+    width: 70%;
+    height: 100%;
+
+    background: blue;
+}
 </style>
