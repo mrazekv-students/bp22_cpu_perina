c3cc4879d83e285ca74c3300399643540f3ef613
Revarin <dan.perina2@gmail.com>
2022-02-11 13:58:33 +0100

    Add assembler compiler (WIP)

--- src/scripts/Compiler.js
+++ src/scripts/Compiler.js
@@ -4,6 +4,8 @@ import Instruction from "./Instruction";
 
 const ERROR_UNKNOWN = 10;
 const ERROR_NL = 11;
+const ERROR_MISSING_PARAM = 12;
+const ERROR_PARAM = 13;
 
 // Starts compilation of `code`, resulting internal code is stored in `instructionList`
 // Returns result object
@@ -26,9 +28,9 @@ function processInstruction(codeList, instructionList) {
     var instruction = codeList.pop();
 
     // Check if EOL
-    var instObject = {inst: instruction};
+    var instObject = {str: instruction};
     var isNewLine = checkNewLine(instObject);
-    instruction = instObject.inst;
+    instruction = instObject.str;
 
     switch (instruction.toUpperCase()) {
         case Instruction.HALT.name:
@@ -128,12 +130,62 @@ function processParameterlessInstruction(instruction, isNewLine, instructionList
     return result;
 }
 
+// Processes instruction with address parameter
+// Returns result object
+function processAddressInstrution(instruction, isNewLine, codeList, instructionList) {
+    result = { result: 0 };
+
+    if (!isNewLine) {
+        parameter = codeList.pop();
+
+        // Check if EOL
+        var paramObject = {str: parameter};
+        isNewLine = checkNewLine(paramObject);
+
+        if (isNewLine) {
+            // Check if address
+            var isAddress = checkAddress(paramObject);
+
+            if (isAddress) {
+                parameter = parseInt(paramObject.str);
+                instructionList.push({ instruction: instruction, address: parameter });
+            }
+            else
+                result = { result: ERROR_PARAM, message: "Incorrect parameter format"};
+        }
+        else 
+            result = { result: ERROR_NL, message: "Parameter must end with new line" };
+    }
+    else
+        result = { result: ERROR_MISSING_PARAM, message: "Missing parameter"};
+
+    return result;
+}
+
 // Check if string ends with newLine
 // Removes newLine from string
-function checkNewLine(str) {
-    var result = str.inst.endsWith("\n");
+function checkNewLine(obj) {
+    var result = obj.str.endsWith("\n");
     if (result) {
-        str.inst = str.inst.slice(0, -1);
+        obj.str = obj.str.slice(0, -1);
     }
     return result;
+}
+
+// Checks if string is syntax valid address
+// Removes '@' char from the beginning
+function checkAddress(obj) {
+    var result = obj.str.startsWith("@");
+    if (result) {
+        obj.str = obj.str.substring(1);
+
+        result = isNumber(obj.str);
+    }
+    return result
+}
+
+// Check is string is number
+// Source: https://stackoverflow.com/questions/1779013/check-if-string-contains-only-digits
+function isNumber(str) {
+    return /^\d+$/.test(str);
 }
