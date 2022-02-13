ebb20e8977c3f81d5c793c4fe25e7df356e3d32f
Revarin <dan.perina2@gmail.com>
2022-02-13 16:21:17 +0100

    Fixup assembler compiler

--- doc/sources.md
+++ doc/sources.md
@@ -3,3 +3,6 @@
 ## Code editor
 [Vue prism editor](https://github.com/koca/vue-prism-editor/tree/feature/next)
 [Prism highlighter](https://prismjs.com/)
+
+## Vue.js tutorials
+https://stackoverflow.com/questions/55316490/vue-best-practice-for-calling-a-method-in-a-child-component
--- src/scripts/Compiler.js
+++ src/scripts/Compiler.js
@@ -3,15 +3,16 @@
 import Instruction from "./Instruction";
 
 const ERROR_UNKNOWN = 10;
-const ERROR_NL = 11;
-const ERROR_MISSING_PARAM = 12;
-const ERROR_PARAM = 13;
+const ERROR_PARAM = 11;
+
+var instructionNumber = 0;
 
 // Starts compilation of `code`, resulting internal code is stored in `instructionList`
 // Returns result object
-function startCompilation(code, instructionList) {
+export default function startCompilation(code, instructionList) {
     var result = { result: 0 };
-    var codeList = code.split(' ');
+    var codeList = code.split(/\s+/);
+    instructionNumber = 0;
 
     // Compilation loop
     while (codeList.length > 0) {
@@ -25,91 +26,88 @@ function startCompilation(code, instructionList) {
 // Instruction switch
 function processInstruction(codeList, instructionList) {
     var result = { result: 0 };
-    var instruction = codeList.pop();
-
-    // Check if EOL
-    var isNewLine = checkNewLine(instruction);
-    if (isNewLine) instruction.slice(0, -1);
+    var instruction = codeList.shift();
+    instructionNumber++;
 
     switch (instruction.toUpperCase()) {
         case Instruction.HALT.name:
-            result = processParameterlessInstruction(Instruction.HALT.name, isNewLine, instructionList);
+            result = processParameterlessInstruction(Instruction.HALT.name, instructionList);
             break;
 
         case Instruction.NEGATE.name:
-            result = processParameterlessInstruction(Instruction.NEGATE.name, isNewLine, instructionList);
+            result = processParameterlessInstruction(Instruction.NEGATE.name, instructionList);
             break;
 
         case Instruction.ACCDEC.name:
-            result = processParameterlessInstruction(Instruction.ACCDEC.name, isNewLine, instructionList);
+            result = processParameterlessInstruction(Instruction.ACCDEC.name, instructionList);
             break;
 
         case Instruction.ACCINC.name:
-            result = processParameterlessInstruction(Instruction.ACCINC.name, isNewLine, instructionList);
+            result = processParameterlessInstruction(Instruction.ACCINC.name, instructionList);
             break;
 
         case Instruction.NOP.name:
-            result = processParameterlessInstruction(Instruction.NOP.name, isNewLine, instructionList);
+            result = processParameterlessInstruction(Instruction.NOP.name, instructionList);
             break;
 
         case Instruction.OUTP.name:
-            result = processAddressInstrution(Instruction.OUTP.name, isNewLine, codeList, instructionList);
+            result = processAddressInstrution(Instruction.OUTP.name, codeList.shift(), instructionList);
             break;
 
         case Instruction.INP.name:
-            result = processAddressInstrution(Instruction.INP.name, isNewLine, codeList, instructionList);
+            result = processAddressInstrution(Instruction.INP.name, codeList.shift(), instructionList);
             break;
 
         case Instruction.MLOAD.name:
-            result = processValueInstruction(Instruction.MLOAD.name, isNewLine, codeList, instructionList);
+            result = processValueInstruction(Instruction.MLOAD.name, codeList.shift(), instructionList);
             break;
 
         case Instruction.DLOAD.name:
-            result = processAddressInstrution(Instruction.DLOAD.name, isNewLine, codeList, instructionList);
+            result = processAddressInstrution(Instruction.DLOAD.name, codeList.shift(), instructionList);
             break;
 
         case Instruction.ILOAD.name:
-            result = processAddressInstrution(Instruction.ILOAD.name, isNewLine, codeList, instructionList);
+            result = processAddressInstrution(Instruction.ILOAD.name, codeList.shift(), instructionList);
             break;
 
         case Instruction.DSTORE.name:
-            result = processAddressInstrution(Instruction.DSTORE.name, isNewLine, codeList, instructionList);
+            result = processAddressInstrution(Instruction.DSTORE.name, codeList.shift(), instructionList);
             break;
 
         case Instruction.ISTORE.name:
-            result = processAddressInstrution(Instruction.ISTORE.name, isNewLine, codeList, instructionList);
+            result = processAddressInstrution(Instruction.ISTORE.name, codeList.shift(), instructionList);
             break;
 
         case Instruction.BRANCH.name:
-            result = processLabelInstruction(Instruction.BRANCH.name, isNewLine, codeList, instructionList);
+            result = processLabelInstruction(Instruction.BRANCH.name, codeList.shift(), instructionList);
             break;
 
         case Instruction.BRZERO.name:
-            result = processLabelInstruction(Instruction.BRZERO.name, isNewLine, codeList, instructionList);
+            result = processLabelInstruction(Instruction.BRZERO.name, codeList.shift(), instructionList);
             break;
 
         case Instruction.BRPOS.name:
-            result = processLabelInstruction(Instruction.BRPOS.name, isNewLine, codeList, instructionList);
+            result = processLabelInstruction(Instruction.BRPOS.name, codeList.shift(), instructionList);
             break;
 
         case Instruction.BRNEG.name:
-            result = processLabelInstruction(Instruction.BRNEG.name, isNewLine, codeList, instructionList);
+            result = processLabelInstruction(Instruction.BRNEG.name, codeList.shift(), instructionList);
             break;
 
         case Instruction.MADD.name:
-            result = processAddressInstrution(Instruction.MADD.name, isNewLine, codeList, instructionList);
+            result = processAddressInstrution(Instruction.MADD.name, codeList.shift(), instructionList);
             break;
 
         case Instruction.IJUMP.name:
-            result = processLabelInstruction(Instruction.IJUMP.name, isNewLine, codeList, instructionList);
+            result = processLabelInstruction(Instruction.IJUMP.name, codeList.shift(), instructionList);
             break;
 
         case Instruction.LABEL.name:
-            result = processLabelInstruction(Instruction.LABEL.name, isNewLine, codeList, instructionList);
+            result = processLabelInstruction(Instruction.LABEL.name, codeList.shift(), instructionList);
             break;
 
         default:
-            result = { result: ERROR_UNKNOWN, message: "Unknown instruction" };
+            result = { result: ERROR_UNKNOWN, instructionNumber: instructionNumber, message: "Unknown instruction" };
             break;
     }
 
@@ -118,108 +116,75 @@ function processInstruction(codeList, instructionList) {
 
 // Processes instruction with no parameters
 // Returns result object
-function processParameterlessInstruction(instruction, isNewLine, instructionList) {
-    result = { result: 0 };
-
-    if (isNewLine)
-        instructionList.push({ instruction: instruction });
-    else
-        result = { result: ERROR_NL, message: "Instruction must end with new line" };
-
-    return result;
+function processParameterlessInstruction(instruction, instructionList) {
+    instructionList.push({ instruction: instruction });
+    return { result: 0 };
 }
 
 // Processes instruction with address parameter
 // Returns result object
-function processAddressInstrution(instruction, isNewLine, codeList, instructionList) {
-    result = { result: 0 };
+function processAddressInstrution(instruction, parameter, instructionList) {
+    var result = { result: 0 };
 
-    if (!isNewLine) {
-        parameter = codeList.pop();
-
-        // Check if EOL
-        if (checkNewLine(parameter)) {
-            parameter = parameter.slice(0, -1);     // Remove '\n'
-
-            // Check if address
-            if (checkAddress(parameter)) {
-                parameter = parseInt(parameter.substring(1));
-                instructionList.push({ instruction: instruction, address: parameter });
-            }
-            else
-                result = { result: ERROR_PARAM, message: "Incorrect parameter format"};
-        }
-        else 
-            result = { result: ERROR_NL, message: "Parameter must end with new line" };
+    // Check if address
+    if (isAddress(parameter)) {
+        parameter = parseInt(parameter.substring(1));
+        instructionList.push({ instruction: instruction, address: parameter });
     }
     else
-        result = { result: ERROR_MISSING_PARAM, message: "Missing parameter"};
+        result = { result: ERROR_PARAM, instructionNumber: instructionNumber, message: "Incorrect parameter format"};
 
     return result;
 }
 
 // Processes instruction with value parameter
 // Returns result object
-function processValueInstruction(instruction, isNewLine, codeList, instructionList) {
-    result = { result: 0 };
+function processValueInstruction(instruction, parameter, instructionList) {
+    var result = { result: 0 };
 
-    if (!isNewLine) {
-        parameter = codeList.pop();
-
-        // Check if EOL
-        if (checkNewLine(parameter)) {
-            parameter = parameter.slice(0, -1);     // Remove '\n'
-
-            // Check if value
-            if (isNumber(parameter)) {
-                parameter = parseInt(parameter);
-                instructionList.push({ instruction: instruction, value: parameter });
-            }
-            else
-                result = { result: ERROR_PARAM, message: "Incorrect parameter format"};
-        }
-        else
-            result = { result: ERROR_NL, message: "Parameter must end with new line" };
+    // Check if value
+    if (isNumber(parameter)) {
+        parameter = parseInt(parameter);
+        instructionList.push({ instruction: instruction, value: parameter });
     }
     else
-        result = { result: ERROR_MISSING_PARAM, message: "Missing parameter"};
+        result = { result: ERROR_PARAM, instructionNumber: instructionNumber, message: "Incorrect parameter format" };
 
     return result;
 }
 
 // Processes instruction with label parameter
 // Returns result object
-function processLabelInstruction(instruction, isNewLine, codeList, instructionList) {
-    result = { result: 0 };
+function processLabelInstruction(instruction, parameter, instructionList) {
+    var result = { result: 0 };
 
-    if (!isNewLine) {
-        parameter = codeList.pop();
-
-        // Check if EOL
-        if (checkNewLine(parameter)) {
-            parameter = parameter.slice(0, -1);     // Remove '\n'
-            instructionList.push({ instruction: instruction, label: parameter })
-        }
-        else
-            result = { result: ERROR_NL, message: "Parameter must end with new line" };
+    // Check if not instruction keyword
+    if (!isKeyword(parameter)) {
+        instructionList.push({ instruction: instruction, label: parameter })
     }
     else
-        result = { result: ERROR_MISSING_PARAM, message: "Missing parameter"};
-}
-
-// Check if string ends with newLine
-function checkNewLine(str) {
-    return str.endsWith("\n");
+        result = { result: ERROR_PARAM, instructionNumber: instructionNumber, message: "Parameter must not be keyword" };
+    
+    return result;
 }
 
 // Checks if string is syntax valid address
-function checkAddress(str) {
+function isAddress(str) {
     var result = str.startsWith("@");
     if (result) {
         str = str.substring(1);
         result = isNumber(str);
     }
     return result
+}
+
+// Check if string is instruction keyword
+function isKeyword(str) {
+    var result = false;
+    Object.keys(Instruction).forEach(instruction => {
+        if (instruction === str.toUpperCase()) result = true;
+    });
+    return result;
 }
 
 // Check is string is number
