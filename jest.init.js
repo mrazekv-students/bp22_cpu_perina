43db576e8a83a34ab4cbadef08723d2c569c86b9
Revarin <dan.perina2@gmail.com>
2022-09-06 16:26:43 +0200

    Fixup cpu test (add tooltip directive)

--- jest.init.js
+++ jest.init.js
@@ -1,4 +1,6 @@
 import { config } from "@vue/test-utils";
+import { VTooltip } from 'floating-vue';
+  
 config.global.config.globalProperties = {
     animations: { enable: true },
     times: { instructionWait: 0, connectorFill: 0, connectorFade: 0, highlightFade: 0 },
@@ -6,4 +8,7 @@ config.global.config.globalProperties = {
     cycleCosts: { cacheCheck: 2, cacheAccess: 10, ramAccess: 200 },
     memorySize: { cache: 4, ram: 16 },
     colors: { secondaryColor: "#8b161c", secondaryColorLight: "#cc1717", fontColor: "#D6D6D6", infoColor: "#3f60bb" }
+};
+config.global.directives = {
+    "tooltip": VTooltip
 };
